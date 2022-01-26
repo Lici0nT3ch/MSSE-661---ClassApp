752fb49a2c4e28fd3da7cc65ed561254485a3763
Alicia Boone <aliciaboonepro@gmail.com>
2022-01-26 18:56:36 -0500

    week 2 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { SizePipe } from './shared/pipes/size.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PizzaAppComponent,
+    SizePipe
   ],
   imports: [
     BrowserModule,
@@ -15,4 +21,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
